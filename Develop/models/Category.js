23b81340789244c6fbe53e0248fac43491145b90
Paul Sherrill <pauldsherrill@yahoo.com>
2024-05-18 13:10:59 -0500

    initial commit

---
+++ Develop/models/Category.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    category_name: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
